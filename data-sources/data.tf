99f7d435b7bca7d781f3db042afb8e715ed7d141
unknown <vikas.arisela@gmail.com>
2026-03-15 15:57:12 +0400

    terraform basics

---
+++ data-sources/data.tf
@@ -0,0 +1,8 @@
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
